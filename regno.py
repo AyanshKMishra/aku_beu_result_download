301d02ce276b03b23b1e2ab743ff91283bf10e35
AyanshKMishra <vikesh80100@gmail.com>
2022-12-07 13:24:09 +0530

    1st commit

---
+++ regno.py
@@ -0,0 +1,88 @@
+import pdfkit
+from PyPDF2 import PdfFileMerger
+import os, argparse
+#from merger import *
+def merge_pdfs(input_files: list, page_range: tuple, output_file: str, bookmark: bool = True):
+    merger = PdfFileMerger(strict = False)
+    for input_file in input_files:
+        bookmark_name = os.path.splitext(os.path.basename(input_file))[0] if bookmark else None
+        merger.append(fileobj = open(input_file, 'rb'), pages = page_range, import_bookmarks = False, bookmark = bookmark_name)
+
+    merger.write(fileobj = open(output_file, 'wb'))
+    merger.close()
+
+regno = 20102113001
+#for i in range(0,60):
+#    print (regno)
+#    return regno
+#    regno = regno + 1
+#res_link = 'http://results.akuexam.net/ResultsBTechBPharm6thSemPub2020.aspx?Sem=VI&RegNo='
+
+res_link = 'http://results.akuexam.net/ResultsBTechBPharm3rdSemPub2021.aspx?Sem=III&RegNo='
+
+regno_list = []
+for i in range(0,54):
+    regno_list.append(regno)
+    regno = regno + 1
+
+#regno_list.remove(19102113001)
+#regno_list.remove(19102113008)
+#regno_list.remove(19102113024)
+#regno_list.remove(19102113041)
+#regno_list.remove(19102113047)
+
+regno_le = []
+regno_le_var = 21102113901
+
+for i in range (0, 16):
+    regno_le.append(regno_le_var)
+    regno_le_var = regno_le_var + 1
+
+
+regno_list = regno_list + regno_le
+#
+
+#print(regno_list)
+string_regno = [str(x) for x in regno_list]
+#print (string_regno)
+regno_link = [res_link + x for x in string_regno]
+#`print(regno_link)
+#for x in regno_link:
+#    print(f'{x}\n')
+
+#for x,y in regno_link,string_regno:
+#    pdfkit.from_url(x, y, verbose=True)
+
+res = [regno_link, string_regno]
+regno_pdf = [x + '.pdf' for x in string_regno]
+
+#print(res)
+#pdfkit.from_url(regno_link, 'out.pdf')
+
+for i in range(len(regno_link)):
+    if os.path.exists(regno_pdf[i]):
+        continue
+    else:
+        pdfkit.from_url(regno_link[i], regno_pdf[i])
+        print(f'{regno_pdf[i]} has been downloaded')
+        i = i + 1
+
+
+
+
+#pdfs = [a for a in os.listdir() if a.endswith('.pdf')]
+pdfs = regno_pdf
+pdfs.sort()
+merge_pdfs(input_files=pdfs, page_range = (0,1), output_file = '2k19_me_3rd_sem.pdf')
+
+remf = regno_pdf
+for f in remf:
+    os.remove(f)
+#pdfs = regno_pdf
+#merger = PdfFileMerger()
+#
+#for pdf in pdfs:
+#    merger.append(pdf)
+#
+#merger.write("all result.pdf")
+#merger.close()
