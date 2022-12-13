b964e30836cb7b88384065beee7507c4450dd5eb
AyanshKMishra <vikesh80100@gmail.com>
2022-12-13 17:27:39 +0530

    ..

--- regno.py
+++ regno.py
@@ -1,7 +1,6 @@
 import pdfkit
 from PyPDF2 import PdfFileMerger
 import os, argparse
-#from merger import *
 def merge_pdfs(input_files: list, page_range: tuple, output_file: str, bookmark: bool = True):
     merger = PdfFileMerger(strict = False)
     for input_file in input_files:
@@ -11,25 +10,29 @@ def merge_pdfs(input_files: list, page_range: tuple, output_file: str, bookmark:
     merger.write(fileobj = open(output_file, 'wb'))
     merger.close()
 
-regno = 20102113001 # enter the starting registration no.
+regno = 16102113001# enter the starting registration no.
 
-res_link = 'http://results.akuexam.net/ResultsBTechBPharm3rdSemPub2021.aspx?Sem=III&RegNo=' # enter the result link without registration no.
+res_link = 'https://akuexam.net/results/ResultsBTechPharm6thSemPub19.aspx?Sem=VI&RegNo=' # enter the result link without registration no.
 
 regno_list = []
-for i in range(0,54): # enter the result range
+for i in range(0,32): # enter the result range
     regno_list.append(regno)
     regno = regno + 1
 
 
 regno_le = []
-regno_le_var = 21102113901 # enter the starting registration no. of le students, if any. if none comment out relevant portions
+regno_le_var = 17102113901 # enter the starting registration no. of le students, if any. if none comment out relevant portions
 
-for i in range (0, 16): # enter the range for the le students
+for i in range (0, 4): # enter the range for the le students
     regno_le.append(regno_le_var)
     regno_le_var = regno_le_var + 1
 
 
 regno_list = regno_list + regno_le
+regno_list.append(15102113033)
+regno_list.append(16102113904)
+regno_list.sort()
+
 
 string_regno = [str(x) for x in regno_list]
 regno_link = [res_link + x for x in string_regno]
@@ -51,7 +54,7 @@ for i in range(len(regno_link)):
 
 pdfs = regno_pdf
 pdfs.sort()
-merge_pdfs(input_files=pdfs, page_range = (0,1), output_file = '2k19_me_3rd_sem.pdf') # enter the name for the output file in the output_file argument
+merge_pdfs(input_files=pdfs, page_range = (0,1), output_file = '2k16_me_6th_sem.pdf') # enter the name for the output file in the output_file argument
 
 remf = regno_pdf
 for f in remf:
